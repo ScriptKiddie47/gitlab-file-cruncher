d2d50b0a64daebce17fd95a53b9808c99ca32722
syndicate <ritambala@live.com>
2025-03-06 13:20:57 +0530

    skeleton-setup

---
+++ src/main/java/com/scriptkiddie/gitlab/file/cruncher/GitlabFileCruncherApplication.java
@@ -0,0 +1,13 @@
+package com.scriptkiddie.gitlab.file.cruncher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabFileCruncherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabFileCruncherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scriptkiddie/gitlab/file/cruncher/GitlabFileCruncherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scriptkiddie.gitlab.file.cruncher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabFileCruncherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
